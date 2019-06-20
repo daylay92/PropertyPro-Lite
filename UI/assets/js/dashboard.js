be4a60cd2f1a21b0edfcdeccd9cebd6ca5cbcf71
Ayodele <daylay92@yahoo.com>
2019-06-20 22:09:08 +0000

    feature(manage-properties): add manage-property page [Finishes #166649858]

--- UI/assets/js/dashboard.js
+++ UI/assets/js/dashboard.js
@@ -31,7 +31,35 @@ const showSideNav = ()=> {
         mainOverLay.classList.remove('dashboard-main__overlay--show');
     }
 }
+//Paginates Manage Properties
+const selectNextPropertyPage = () => {
+    const prevBtn = document.querySelector('.pagination-dashboard__prev');
+    const nextBtn = document.querySelector('.pagination-dashboard__next');
+    const currentPageNum = document.querySelector('.pagination-dashboard__current');
+    const lastPageNum = document.querySelector('.pagination-dashboard__count');
+    if(!prevBtn) return;
+    if(!nextBtn) return;
+    prevBtn.onclick = (e) => {
+        const diff = parseInt(currentPageNum.textContent) - 1;
+        if(diff === 1)
+            prevBtn.classList.add('pagination-dashboard__btn--disabled');
+        if(parseInt(lastPageNum.textContent) === parseInt(currentPageNum.textContent))
+            nextBtn.classList.remove('pagination-dashboard__btn--disabled');
+        currentPageNum.textContent --;
+        e.preventDefault();
+    }
+    nextBtn.onclick = (e) => {
+        if(parseInt(currentPageNum.textContent) >= 1)
+            prevBtn.classList.remove('pagination-dashboard__btn--disabled');
+        const diff = parseInt(lastPageNum.textContent) - parseInt(currentPageNum.textContent);
+        if(diff === 1)
+            nextBtn.classList.add('pagination-dashboard__btn--disabled');
+        currentPageNum.textContent ++;
+        e.preventDefault();
+    }
+}
 const startDashApp = () => {
     showSideNav();
+    selectNextPropertyPage();
 }
 startDashApp();

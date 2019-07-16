dc79e753ba9044de9e4f7fcd3d7a26a2e1396bb8
daylay92 <daylay92@yahoo.com>
2019-07-16 17:03:18 +0100

    chore(post-property): modify price validation rules [Finishes #167322332]

--- API/src/middlewares/patch-property-validation.js
+++ API/src/middlewares/patch-property-validation.js
@@ -1,5 +1,6 @@
 import { check } from 'express-validator';
 import PostProperty from './post-property-validation';
+import Helpers from '../utils/helpers';
 
 export default class PutProperty extends PostProperty {
   static validate() {
@@ -13,6 +14,7 @@ export default class PutProperty extends PostProperty {
         .isLength({ min: 3, max: 15 })
         .withMessage('should be between 3-15 characters long')
         .trim()
+        .customSanitizer(Helpers.toInt)
         .matches(/^\d+(\.|\d)\d\d$/)
         .withMessage('should be a float or numbers e.g 5000.00 or 5000')
         .escape()
